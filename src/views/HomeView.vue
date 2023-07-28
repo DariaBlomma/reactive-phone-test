391f53297cce45b4700e307f3fd512886fd2d692
Дарья Козина <kozina.flowers@yandex.ru>
2023-07-28 12:48:37 +0300

    imported icons from figma

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,51 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home view</h1>
+	  <RouterLink :to="{ name: 'learning'}">Learning link</RouterLink>
+
+	  <IconCrown />
+	  <IconFire />
+	  <IconGraduationCap />
+	  <IconShevronCircleRight />
+	  <IconLExam />
+	  <IconRocketLaunch />
+	  <IconTVPlay />
+	  <IconRoadTurn />
+	  <IconGameController />
+	  <IconPen />
+	  <IconChartBar />
+	  <IconBookText />
+	  <IconBookmarksUser />
+	  <IconHome />
+	  <IconRoute />
+	  <IconRanking />
+	  <IconUser />
+	  <IconArrow />
+	  <IconCheckCircle />
   </main>
 </template>
+
+<script setup lang="ts">
+import IconCrown from '@/ui/icons/IconCrown.vue';
+import IconFire from '@/ui/icons/IconFire.vue';
+import IconGraduationCap from '@/ui/icons/IconGraduationCap.vue';
+import IconShevronCircleRight from '@/ui/icons/IconShevronCircleRight.vue';
+import IconLExam from '@/ui/icons/IconLExam.vue';
+import IconRocketLaunch from '@/ui/icons/IconRocketLaunch.vue';
+import IconTVPlay from '@/ui/icons/IconTVPlay.vue';
+import IconRoadTurn from '@/ui/icons/IconRoadTurn.vue';
+import IconGameController from '@/ui/icons/IconGameController.vue';
+import IconPen from '@/ui/icons/IconPen.vue';
+import IconChartBar from '@/ui/icons/IconChartBar.vue';
+import IconBookText from '@/ui/icons/IconBookText.vue';
+import IconBookmarksUser from '@/ui/icons/IconBookmarksUser.vue';
+import IconHome from '@/ui/icons/IconHome.vue';
+import IconRoute from '@/ui/icons/IconRoute.vue';
+import IconRanking from '@/ui/icons/IconRanking.vue';
+import IconUser from '@/ui/icons/IconUser.vue';
+import IconArrow from '@/ui/icons/IconArrow.vue';
+import IconCheckCircle from '@/ui/icons/IconCheckCircle.vue';
+</script>
+
+<style scoped lang="scss">
+</style>
